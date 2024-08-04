1069e671563c243a0c430d360559b646115f0ee0
ASHOK KUMAR REDDY kota <kotaashok331@gmail.com>
2024-08-04 15:27:28 +0530

    first commit

---
+++ Library-Management-Project-main/gestionbib/email_config.py
@@ -0,0 +1,6 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  # For TLS
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kotaashok331@gmail.com'  # Your Gmail address
+EMAIL_HOST_PASSWORD = 'eiaq mgsf dupa tfmy'  # Your Gmail password or App Password
